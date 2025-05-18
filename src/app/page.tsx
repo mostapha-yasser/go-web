dfca98b325c4823721621a898d37f313c39dddea
Mostapha Yasser <mostaphayasser18@gmail.com>
2025-05-18 18:49:38 +0300

    intial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-<div className="min-h-screen text-5xl flex justify-center items-center">
+<div className="min-h-screen text-5xl flex justify-center item">
 
 
 
